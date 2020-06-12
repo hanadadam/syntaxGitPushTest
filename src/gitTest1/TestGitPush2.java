e9f56fe66881c416c1ddbd422e9b19f075e73a58
hanadadam <hanad.adam5@gmail.com>
2020-06-11 21:50:42 -0400

    second code push

---
+++ src/gitTest1/TestGitPush2.java
@@ -0,0 +1,7 @@
+package gitTest1;
+
+public class TestGitPush2 {
+public static void main(String[] args) {
+	System.out.println("what it do");
+}
+}
--- src/gitTest1/testGitPush1.java
+++ src/gitTest1/testGitPush1.java
@@ -2,4 +2,7 @@ package gitTest1;
 
 public class testGitPush1 {
 
+	public static void main(String[] args) {
+		System.out.println("Syntax Technologies");
+	}
 }
